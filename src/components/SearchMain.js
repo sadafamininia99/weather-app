a9867bf5942af7629af5d9cedcee0ed1bb58d94b
sadafamininia99 <aminicomputer96@gmail.com>
2022-02-10 18:38:16 +0330

    get value from input

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import '../components/style.css'
+import React, { useState } from "react";
+import "../components/style.css";
 
 function SearchMain() {
+  const [serachTerm, seSearchTerm] = useState("tehran");
+  console.log(serachTerm);
+
   return (
-    <div className='wrap'>
-      <div className='search'>
-        <input type='search' placeholder='Search City ' id="search"/>
+    <div className="wrap">
+      <div className="search">
+        <input
+          type="search"
+          placeholder="Search City... "
+          id="search"
+          value={serachTerm}
+          onChange={(e) => seSearchTerm(e.target.value)}
+        />
       </div>
-    <button className='searchButton'>
-      Search
-    </button>
+      <button className="searchButton">Search</button>
     </div>
-  )
-
-
-
+  );
 }
 
 export default SearchMain;
 
 // 562a97288069061860b1c44b209f65d3 code
-// dbff20676c8bb0c8ab7ac4abb57b0d68 
+// dbff20676c8bb0c8ab7ac4abb57b0d68
