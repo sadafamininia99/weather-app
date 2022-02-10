6530faca51ed8ec310e58b7c63132d6087b4e5b3
sadafamininia99 <aminicomputer96@gmail.com>
2022-02-10 18:26:39 +0330

    search component

---
+++ src/components/SearchMain.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function SearchMain() {
+  return (
+    <div className='wrap'>
+      <div className='search'>
+        <input type='search' placeholder='Type City Name' id="search"/>
+      </div>
+    <button className='searchButton'>
+      Search city
+    </button>
+    </div>
+  )
+
+
+
+}
+
+export default SearchMain;
+
+// 562a97288069061860b1c44b209f65d3 code
+// dbff20676c8bb0c8ab7ac4abb57b0d68 
