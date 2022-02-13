fa4144115426ea6167d7e84033a01eb1b80156c4
sadafamininia99 <aminicomputer96@gmail.com>
2022-02-13 18:46:13 +0330

    sunrise icon

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -8,13 +8,24 @@ function WeatherDetails() {
           <i className="wi-day-sunny"></i>
         </div>
         <div className="weatherInfo">
-          <span>23.99</span>
-        </div>
-        <div className="description">
-          <div className="weatherCondition">sunny</div>
-          <div className="place">Tehran ,IR</div>
+          <div className="temperature">
+            <span>23.99</span>
+          </div>
+          <div className="description">
+            <div className="weatherCondition">sunny</div>
+            <div className="place">Tehran ,IR</div>
+          </div>
         </div>
         <div className="date">{new Date().toLocaleString()} </div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+            </div>
+          </div>
+        </div>
       </article>
     </>
   );
