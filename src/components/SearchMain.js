b2baeb52d528c922015dccbd1a7e7231c73d8c5a
sadafamininia99 <aminicomputer96@gmail.com>
2022-02-14 11:32:22 +0330

    dependency array

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,36 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../components/style.css";
 import WeatherDetails from "./WeatherDetails";
 
 function SearchMain() {
   const [serachTerm, seSearchTerm] = useState("tehran");
 
-
   console.log(serachTerm);
-//useefect 
-//async function
-//promises
+  //useefect
+  //async function
+  //promises
+  //try catch
+
+  useEffect(() => {
+    //function
+    
+
+  }, []);
+  //dependecy array :/
+
 
   return (
     <>
-    <div className="wrap">
-      <div className="search">
-        <input
-          type="search"
-          placeholder="Search City... "
-          id="search"
-          value={serachTerm}
-          onChange={(e) => seSearchTerm(e.target.value)}
-        />
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search City... "
+            id="search"
+            value={serachTerm}
+            onChange={(e) => seSearchTerm(e.target.value)}
+          />
+        </div>
+        <button className="searchButton" onClick={getWeatherInfo}>
+          Search
+        </button>
       </div>
-      <button className="searchButton" onClick={getWeatherInfo}>Search</button>
-    </div>
-    <WeatherDetails/>
+      <WeatherDetails />
     </>
   );
 }
-
-
-
 
 export default SearchMain;
