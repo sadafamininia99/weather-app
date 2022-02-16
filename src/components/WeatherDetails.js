60d061b8761c9cb62e09db15e4e7d20d7fed16df
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2022-02-16 14:25:51 +0330

    Update WeatherDetails.js
      conveting the seconds in time

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,22 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function WeatherDetails() {
+function WeatherDetails({
+  temp,
+  humidity,
+  pressure,
+  weatherType,
+  name,
+  speed,
+  country,
+  sunset,
+}) {
+  const [weatherState, setWeatherState] = useState("");
+  useEffect(() => {
+    if (weatherType) {
+      switch (weatherType) {
+        case "Clouds":
+          setWeatherState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        case "Mist":
+          setWeatherState("wi-dust");
+          break;
+        case "Rain":
+          setWeatherState("wi-day-rain");
+          break;
+
+        default:
+          setWeatherState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weatherType]);
+
+  //conveting the seconds in time
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
   return (
     <>
       <article className="widget">
         <div className="weatherIcon">
-          <i className="wi-day-sunny"></i>
+          <i className={`wi ${weatherState}`}></i>
         </div>
         <div className="weatherInfo">
           <div className="temperature">
-            <span>23.99</span>
+            <span>{temp}&deg;</span>
           </div>
           <div className="description">
-            <div className="weatherCondition">sunny</div>
-            <div className="place">Tehran ,IR</div>
+            <div className="weatherCondition">{weatherType}</div>
+            <div className="place">
+              {name}, {country}
+            </div>
           </div>
         </div>
-        <div className="date">{new Date().toLocaleString()} </div>
+        <div className="date">{new Date().toLocaleString()}</div>
         <div className="extra-temp">
           <div className="temp-info-minmax">
             <div className="two-sided-section">
@@ -24,8 +66,8 @@ function WeatherDetails() {
                 <i className={"wi wi-sunset"}></i>
               </p>
               <p className="extra-info-leftside">
-                6:30 PM <br />
-                sunset
+                {timeStr} PM <br />
+                Sunset
               </p>
             </div>
 
@@ -34,26 +76,30 @@ function WeatherDetails() {
                 <i className={"wi wi-humidity"}></i>
               </p>
               <p className="extra-info-leftside">
-                444 <br />
-                humidity
+                {humidity} <br />
+                Humidity
               </p>
             </div>
+          </div>
+
+          <div className="weather-extra-info">
             <div className="two-sided-section">
               <p>
                 <i className={"wi wi-rain"}></i>
               </p>
               <p className="extra-info-leftside">
-                433 <br />
-                pressure
+                {pressure} <br />
+                Pressure
               </p>
             </div>
+
             <div className="two-sided-section">
               <p>
                 <i className={"wi wi-strong-wind"}></i>
               </p>
               <p className="extra-info-leftside">
-                232 <br />
-                speed
+                {speed} <br />
+                Speed
               </p>
             </div>
           </div>
